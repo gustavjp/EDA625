4353301f6cd838e7ea1ec483c7a912b91d974806
Gustav Jansson Ekstrand <gustav.jp@live.se>
2015-02-15 11:24:13 +0100

    Stuff

--- Projekt1/src/Test2.java
+++ Projekt1/src/Test2.java
@@ -5,16 +5,8 @@ public class Test2 {
         Generator g = new Generator();
         BigInteger b = new BigInteger("2");
         b = b.pow(4);
-<<<<<<< HEAD
         b = b.add(new BigInteger("7"));
         System.out.println("b is " + b.toString());
         System.out.println(b + " is " + g.isPrimeA(b));
     }
 }
-=======
-        b = b.add(BigInteger.ONE);
-        System.out.println("b is " + b.toString());
-        System.out.println(b + " is " + g.isPrime(b));
-    }
-}
->>>>>>> efbcdf43bec550e4322d63e2946a116abbcc3f8a
