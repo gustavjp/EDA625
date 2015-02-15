27436a726296e79b24852b26b7245a6f1f5958a4
Gustav Jansson Ekstrand <gustav.jp@live.se>
2015-02-16 00:18:15 +0100

    Stuff

--- Projekt1/src/Test3.java
+++ Projekt1/src/Test3.java
@@ -8,8 +8,8 @@ import java.util.Random;
 
 public class Test3 {
     public static void main(String[] args) {
-        secondMethod();
         firstMethod();
+        secondMethod();
     }
 
     private static void firstMethod() {
