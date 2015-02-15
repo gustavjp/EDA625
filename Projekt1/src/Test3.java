ce6aa26d1abef06832ef0e145e82288061dfeb2a
Gustav Jansson Ekstrand <gustav.jp@live.se>
2015-02-15 12:19:54 +0100

    Added sieve

--- Projekt1/src/Test3.java
+++ Projekt1/src/Test3.java
@@ -6,9 +6,6 @@ import java.math.BigInteger;
 import java.util.Objects;
 import java.util.Random;
 
-/**
- * Created by gus on 2015-02-15.
- */
 public class Test3 {
     public static void main(String[] args) {
         firstMethod();
@@ -18,52 +15,52 @@ public class Test3 {
     private static void firstMethod() {
         try {
             String output;
-            File file = new File("/home/gus/code/EDA625/data/collectionFirst.txt");
-            FileWriter fw = new FileWriter(file.getAbsoluteFile());
             Generator g = new Generator();
             BigInteger base;
             BigInteger two = new BigInteger("2");
             BigInteger temp;
             BigInteger addition;
             boolean found;
-            long time1, time2 = 0, time3, tot;
+            long time1 = 0, time2 = 0, time3, tot;
             int n;
-
-            // if file doesn't exists, then create it
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-
-            BufferedWriter bw = new BufferedWriter(fw);
             for(int i = 9; i < 12; i++) {
-                //for(int i = 2; i < 5; i++) {
                 n = 0;
                 tot = 0;
-                base = two.pow((int) Math.pow(2.0, (double) i));
+                base = two.pow((int) Math.pow(2, i));
                 time3 = System.currentTimeMillis();
                 while(n < 10) {
-                    addition = new BigInteger((int) Math.pow(2.0, (double) i), new Random());
+                    found = false;
+                    addition = new BigInteger((int) Math.pow(2, i), new Random());
                     if(addition.mod(new BigInteger("2")).equals(BigInteger.ZERO)) {
                         addition = addition.add(BigInteger.ONE);
                     }
                     temp = base.add(addition);
-                    time1 = System.currentTimeMillis();
-                    found = g.isPrimeA(temp);
-                    time2 = System.currentTimeMillis();
+                    if(sieve(temp)) {
+                        time1 = System.currentTimeMillis();
+                        found = g.isPrimeB(temp);
+                        time2 = System.currentTimeMillis();
+                    }
                     if(found) {
                         tot += time2 - time1;
                         n++;
                     }
                 }
                 System.out.println(tot);
-                output = "For x^(j)s and " + (int) Math.pow(2.0, (double) i) + " bits\n";
+                output = "For x^(j)s and " + (int) Math.pow(2, i) + " bits\n";
                 output += "Total execution time: " + (time2 - time3) + " ms\n";
                 output += "Total checking time: " + Objects.toString(tot, null) + " ms\n";
                 output += "-----------------DONE-----------------\n";
+                File file = new File("/home/gus/code/EDA625/data/meth1" + (int) Math.pow(2, i) +".txt");
+                FileWriter fw = new FileWriter(file.getAbsoluteFile());
+                BufferedWriter bw = new BufferedWriter(fw);
+                // if file doesn't exists, then create it
+                if (!file.exists()) {
+                    file.createNewFile();
+                }
                 bw.write(output);
+                bw.close();
                 System.out.println("Data written to file.");
             }
-            bw.close();
             System.out.println("Done");
         } catch (IOException e) {
             e.printStackTrace();
@@ -73,55 +70,67 @@ public class Test3 {
     private static void secondMethod() {
         try {
             String output;
-            File file = new File("/home/gus/code/EDA625/data/collectionSecond.txt");
-            FileWriter fw = new FileWriter(file.getAbsoluteFile());
             Generator g = new Generator();
             BigInteger base;
             BigInteger two = new BigInteger("2");
             BigInteger temp;
             BigInteger addition;
             boolean found;
-            long time1, time2 = 0, time3, tot;
+            long time1 = 0, time2 = 0, time3, tot;
             int n;
-
-            // if file doesn't exists, then create it
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-
-            BufferedWriter bw = new BufferedWriter(fw);
             for(int i = 9; i < 12; i++) {
-                //for(int i = 2; i < 5; i++) {
                 n = 0;
                 tot = 0;
-                base = two.pow((int) Math.pow(2.0, (double) i));
+                base = two.pow((int) Math.pow(2, i));
                 time3 = System.currentTimeMillis();
                 while(n < 10) {
-                    addition = new BigInteger((int) Math.pow(2.0, (double) i), new Random());
+                    found = false;
+                    addition = new BigInteger((int) Math.pow(2, i), new Random());
                     if(addition.mod(new BigInteger("2")).equals(BigInteger.ZERO)) {
                         addition = addition.add(BigInteger.ONE);
                     }
                     temp = base.add(addition);
-                    time1 = System.currentTimeMillis();
-                    found = g.isPrimeB(temp);
-                    time2 = System.currentTimeMillis();
+                    if(sieve(temp)) {
+                        time1 = System.currentTimeMillis();
+                        found = g.isPrimeB(temp);
+                        time2 = System.currentTimeMillis();
+                    }
                     if(found) {
                         tot += time2 - time1;
                         n++;
                     }
                 }
                 System.out.println(tot);
-                output = "For x^(2)s and " + (int) Math.pow(2.0, (double) i) + " bits\n";
+                output = "For x^(2)s and " + (int) Math.pow(2, i) + " bits\n";
                 output += "Total execution time: " + (time2 - time3) + " ms\n";
                 output += "Total checking time: " + Objects.toString(tot, null) + " ms\n";
                 output += "-----------------DONE-----------------\n";
+                File file = new File("/home/gus/code/EDA625/data/meth2" + (int) Math.pow(2, i) +".txt");
+                FileWriter fw = new FileWriter(file.getAbsoluteFile());
+                BufferedWriter bw = new BufferedWriter(fw);
+                // if file doesn't exists, then create it
+                if (!file.exists()) {
+                    file.createNewFile();
+                }
                 bw.write(output);
+                bw.close();
                 System.out.println("Data written to file.");
             }
-            bw.close();
             System.out.println("Done");
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    
+    public static boolean sieve(BigInteger b) {
+        if(b.mod(new BigInteger("2")).equals(BigInteger.ZERO)) {
+            return false;
+        }
+        for(int i = 3; i < 65000; i += 2) {
+            if(b.mod(new BigInteger(Integer.toString(i))).equals(BigInteger.ZERO)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
